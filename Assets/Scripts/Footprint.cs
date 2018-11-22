536925bb01e9d1cf6a79d56757b41cd31a10f02e
이영기 <t9rres@ajou.ac.kr>
2018-11-22 14:46:04 +0900

    평상시 상태 커서 추가 및 이동 명령 시 발자국 새성

---
+++ Assets/Scripts/Footprint.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Footprint : MonoBehaviour {
+	public float deadTime;
+	SpriteRenderer spriteRenderer;
+	float timer;
+
+	void Awake() {
+		timer = 0.0f;
+		spriteRenderer = GetComponent<SpriteRenderer> ();
+	}
+
+	void Update() {
+		timer += Time.deltaTime;
+		Color color = spriteRenderer.color;
+		color.a -= Time.deltaTime / deadTime;
+		spriteRenderer.color = color;
+		if (timer >= deadTime) {
+			Destroy (gameObject);
+		}
+	}
+}
