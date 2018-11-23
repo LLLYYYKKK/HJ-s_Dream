acdb786453d8f60dbd06afc0a403b3864fc6ddcb
이영기 <t9rres@ajou.ac.kr>
2018-11-23 17:41:33 +0900

    스킬 선택 사운드 UI에서 나오게함

--- Assets/Scripts/Skill/SkillManager.cs
+++ Assets/Scripts/Skill/SkillManager.cs
@@ -20,8 +20,6 @@ public class SkillManager : MonoBehaviour {
 
 	public AudioClip skillCoolTimeSound;
 
-	public AudioClip setSkillSound;
-
 	public AudioClip obtainSkillSound;
 
 	void Awake () {
@@ -141,13 +139,6 @@ public class SkillManager : MonoBehaviour {
 
 		canUseSkills [skillIndex] = obtainedSkills [obtainedSkillIndex];
 		uiCanvas.SetCanUseSkill (skillIndex, canUseSkills [skillIndex].GetComponent<Skill> ());
-	}
-
-	public void SetCanUseSkill(int skillIndex, int obtainedSkillIndex, bool withSound) {
-		SetCanUseSkill (skillIndex, obtainedSkillIndex);
-		if (withSound) {
-			audioSource.PlayOneShot (setSkillSound);
-		}
 	}
 
 	public void SetCanUseSkillNull(int skillIndex) {
